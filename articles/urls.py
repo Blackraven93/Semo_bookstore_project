0c367ffa2a7afbf6604fee0fcfdc0095788b0543
errorfixie <65066542+errorfixie@users.noreply.github.com>
2020-07-07 11:13:31 +0900

    articles/author 작업중

--- articles/urls.py
+++ articles/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-
+from . import views
 app_name = 'articles'
 
 urlpatterns = [
-
+    path("author/",views.ThisMonthAuthorListView ,name="author"),
+    # path("book/",views. ,name="book"),
+    # path("event/",views. ,name="event"),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Article
 
-# Create your views here.
+
+# 이달의 도서 - listView
+# 필요 요소: 도서 사진, 기사 제목, 기사 내용, 등록일자, 도서카테고리, 기사카테고리 
+# 기능 : 기사카테고리 드롭박스, 페이지네이션, 도서카테고리 박스, 등록일자 년 박스, 등록일자 월 박스
+
+# 이달의 행사 - listView
+# 필요 요소: 행사사진, 행사 이름, 기사 내용, 등록일자, 기사카테고리 
+# 기능 : 기사카테고리 드롭박스, 검색도구, 페이지네이션
+
+# 이달의 작가 - detailView
+# 필요 요소: 행사 사진, 작가사진, 작가등등 작가Id, 기사제목, 기사내용, 기사카테고리 
+# 기능 : 기사카테고리 드롭박스
+def ThisMonthAuthorListView(request):
+    author_info = Article.objects.all()
+    return render(request,"articles/author.html",{"author_info":author_info},)
